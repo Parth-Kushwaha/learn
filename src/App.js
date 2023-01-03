043560dcea3fed3050ea66f81a9e67cb505b6b4d
Parth-Kushwaha <parthkushwaha2003@gmail.com>
2023-01-03 20:57:41 +0530

    aaa

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from "./components/textForm";
+import About from "./components/About";
 
 function App() {
   return (
     <>
      <Navbar title="TextUtensils " h1="HOME" h2="About" bar="Search"/>
-     <TextForm/>
+     {/* <TextForm/> */}
+     <About/>
     </>
    
   );
--- src/components/textForm.js
+++ src/components/textForm.js
@@ -7,21 +7,46 @@ export default function TextForm() {
         let newText=text.toUpperCase()
         setText(newText);
     }
- 
+
+    const handleLowClick=()=>{
+        let newText=text.toLowerCase()
+        setText(newText);
+    }
+
+    const handleReverse=()=>{
+        setText(text.split("").reverse().join(""));
+    }
+
+    const handleClear=()=>{
+        setText("");
+    }
     const handleOnChange=(event)=>{
         setText(event.target.value);
     }
 
-    const [text, setText]= useState("Enter text here");
+    const [text, setText]= useState("text");
     //setText
     return (
+    <>
     <div className="container mb-3">
-        <div>
-            <label htmlFor="exampleFormControlTextarea1" className="myBox">Enter text to analyze:</label>
+        <div className='container'>
+            <label htmlFor="exampleFormControlTextarea1" className="myBox"><h1>Enter text to analyze:</h1></label>
             <textarea className="form-control" value={text} onChange={handleOnChange} id="exampleFormControlTextarea1" rows="8"></textarea>
         </div>
-        <button className="btn btn-primary" onClick={handleUpClick}>Convert to Upper Case</button>
+            <button className="btn btn-primary my-3 mx-2" onClick={handleUpClick}>Convert to Upper Case</button>
+            <button className="btn btn-primary my-3 mx-2" onClick={handleLowClick}>Convert to Lower Case</button>
+            <button className="btn btn-primary my-3 mx-2" onClick={handleReverse}>Reverse the text</button>
+            <button className="btn btn-primary my-3 mx-2" onClick={handleClear}>To Clear the Text Area</button>
+        </div>
+    <div className="container my-3">
+        <h2>YOUR TEXT SUMMARY</h2>
+        <p>{text.split (" ").length} words and {text.length} characters in your text</p>
+        <p>{ 0.008 * text.split (" ").length} minutes taken to read for an averge reader</p>
+        <h2>PREVIEW OF THE TEXT</h2>
+        <p>{text}</p>
     </div>
+    </>
+    
   );
 }
  
