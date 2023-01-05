07e65480b7530358ef00792b88fce4a1b55a0141
Parth-Kushwaha <parthkushwaha2003@gmail.com>
2023-01-05 12:21:22 +0530

    ll

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Alert(props) {
   return (
-    <div style={{height: "50px"}}>
+    <div style={{height: "60px"}}>
     {props.alert && <div className={`alert alert-success alert-dismissible fade show`} role="alert">
         <strong>{props.alert.type}</strong>: {props.alert.msg}
     </div>}
--- src/components/textForm.js
+++ src/components/textForm.js
@@ -44,7 +44,7 @@ export default function TextForm(props) {
     <div className="container mb-3" style={{color: props.mode==="dark"? 'white':'#042743'}}>
         <div className='container'>
             <label htmlFor="exampleFormControlTextarea1" className="myBox" ><h1>Enter text to analyze:</h1></label>
-            <textarea className="form-control" style={{backgroundColor: props.mode==="dark"? 'grey':'white', color: props.mode==="dark"? 'white':'#042743'}} value={text} onChange={handleOnChange} id="exampleFormControlTextarea1" rows="8"></textarea>
+            <textarea className="form-control" style={{backgroundColor: props.mode==="dark"? '#13466e':'white', color: props.mode==="dark"? 'white':'#042743'}} value={text} onChange={handleOnChange} id="exampleFormControlTextarea1" rows="8"></textarea>
         </div>
             <button className="btn btn-primary my-3 mx-2" onClick={handleUpClick}>Convert to Upper Case</button>
             <button className="btn btn-primary my-3 mx-2" onClick={handleLowClick}>Convert to Lower Case</button>
@@ -55,7 +55,7 @@ export default function TextForm(props) {
     <div className="container my-3" style={{color: props.mode==="dark"? 'white':'#042743'}}>
         <h2>YOUR TEXT SUMMARY</h2>
         <p>{text.split (" ").filter((element)=>{return element.length !== 0}).length} words and {text.length} characters in your text</p>
-        <p>{ 0.008 * text.split (" ").length} minutes taken to read for an averge reader</p>
+        <p>{ 0.008 * text.split (" ").filter((element)=>{return element.length !== 0}).length} minutes taken to read for an averge reader</p>
         <h2>PREVIEW OF THE TEXT</h2>
         <p>{text.length>0 ? text:"Enter some text for preview"}</p>
     </div>
