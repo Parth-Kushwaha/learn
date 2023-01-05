3abc72c398287ce0134030eb1632e11157de504b
Parth-Kushwaha <parthkushwaha2003@gmail.com>
2023-01-05 14:16:05 +0530

    done

--- src/components/textForm.js
+++ src/components/textForm.js
@@ -25,10 +25,10 @@ export default function TextForm(props) {
     }
 
     const handleCopy=()=>{
-        var text=document.getElementById("exampleFormControlTextarea1")
-        text.select();
-        navigator.clipboard.writeText(text.value);
-        document.getSelection().removeAllRanges();
+        // var text=document.getElementById("exampleFormControlTextarea1")
+        // text.select();
+        navigator.clipboard.writeText(text);
+        // document.getSelection().removeAllRanges();
         props.showAlert("Text Copied to Clipboard", "Success!");
     }
 
@@ -57,7 +57,7 @@ export default function TextForm(props) {
         </div>
     <div className="container my-3" style={{color: props.mode==="dark"? 'white':'#042743'}}>
         <h2>YOUR TEXT SUMMARY</h2>
-        <p>{text.split (" ").filter((element)=>{return element.length !== 0}).length} words and {text.length} characters in your text</p>
+        <p>{text.split(/\s+/).filter((element)=>{return element.length !== 0}).length} words and {text.length} characters in your text</p>
         <p>{ 0.008 * text.split (" ").filter((element)=>{return element.length !== 0}).length} minutes taken to read for an averge reader</p>
         <h2>PREVIEW OF THE TEXT</h2>
         <p>{text.length>0 ? text:"NOTHING TO PREVIEW"}</p>
